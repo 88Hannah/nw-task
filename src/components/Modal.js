351e5300215712fa9cca2abec72f069e6b74888d
88Hannah <hannahsangar@gmail.com>
2020-09-09 18:22:59 +0100

    Modal opens, closes and populates

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Context} from '../Context';
 
 function Modal() {
 
 const {allWines, modalWine, toggleModal, isModalOpen, currentType} = useContext(Context);
-
+const [modalTooltipVisibility, setModalTooltipVisibility] = useState("hidden")
+    
 const modalWineInfo = allWines.find(wine => wine.id === modalWine);
 
 var amount, modalText, wineColour, tooltipText
@@ -13,16 +14,29 @@ if (modalWineInfo) {
     modalText = modalWineInfo.description.map((paragraph, id) => (
         <p key={id}>{paragraph}</p>
     ));
-    var {wineColour} = currentType(modalWineInfo.type);
+    var {wineColour, tooltipText} = currentType(modalWineInfo.type);
 }
 
-console.log(isModalOpen)
 const modalDisplay = isModalOpen ? "block" : "none";
+
+const handleMouseEnter = () => {
+    setModalTooltipVisibility("visible");
+};
+
+const handleMouseLeave = () => {
+    setModalTooltipVisibility("hidden");
+};
+
+const handleClick = event => {
+    if(event.target.classList.contains("modal-container")) {
+        toggleModal();
+    };
+};
 
     return (
         
         modalWineInfo ? 
-        <div className="modal-container" style={{display:modalDisplay}}>
+        <div className="modal-container" style={{display:modalDisplay}} onClick={handleClick}>
             <div className="modal">
                 <h2 className="modal__wine-name">{modalWineInfo.name}</h2>
                 <div className="modal__img">
@@ -36,9 +50,15 @@ const modalDisplay = isModalOpen ? "block" : "none";
                     <p className="modal__country">{modalWineInfo.country}</p>
                     <div className="modal__wine-glass-container">
                         <i 
-                            className="fas fa-wine-glass-alt modal__wine-glass" 
-                            style={{color:wineColour}} 
+                            className="fas fa-wine-glass-alt wine-glass modal__wine-glass" 
+                            style={{color:wineColour}}
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
                         ></i>
+                        <span 
+                            className="wine-glass-tooltip" 
+                            style={{visibility:modalTooltipVisibility}}
+                        >{tooltipText}</span>
                     </div>
                     <p className="modal__abv">{modalWineInfo.abv}%</p>
                 </div>
