dc3f8267b5ba694ec7cf943298b6d1e95cf255a9
88Hannah <hannahsangar@gmail.com>
2020-09-10 14:13:46 +0100

    Timings calculated and styling started

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -5,10 +5,10 @@ function Quotes() {
     return (
         <div className="quotes-container">
             <div className="quotes">
-                <p className="quote quote-1">Hello, this is my quote. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote.</p>
-                <p className="quote quote-2">Second one. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote.</p>
-                <p className="quote quote-3">Third time. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote.</p>
-                <p className="quote quote-4">Fourth time. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote. Hello, this is my quote.</p>
+                <p className="quote quote-1"><em>"I have always been delighted with the service that I’ve received from Sangar’s Wine."</em></p>
+                <p className="quote quote-2"><em>"I love receiving my orders from Sangar’s Wines, the quality is always exceptional."</em></p>
+                <p className="quote quote-3"><em>"A friend recommended Sangar’s Wines to me a few years ago and I’ve never looked back."</em></p>
+                <p className="quote quote-4"><em>"Every bottle has been produced to perfection and I savour every drop."</em></p>
             </div>
         </div>
     )
